6bf573bc094d8e77aad39e8edb41930a38b76fcd
Md_Ashikujjaman <ashik35uap@gmail.com>
2025-05-05 11:41:18 +0600

    Check input number interger or decimal

--- src/test/java/md/ashikujjaman/Problem_01_Integer_Or_Decimal_Check.java
+++ src/test/java/md/ashikujjaman/Problem_01_Integer_Or_Decimal_Check.java
@@ -5,8 +5,19 @@ Print "Integer" or "Decimal" accordingly. */
 
 import java.util.Scanner;
 
-public class Number_check {
-    //Class variable or Static variable
-    static int classVar = 10;
+public class Problem_01_Integer_Or_Decimal_Check {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a nimber: ");
+        double number = scanner.nextDouble();
+
+        if (number == Math.floor(number)){ /*Math.floor(number), This function converts
+        any decimal number to the nearest lower integer by removing the decimal part.*/
+            System.out.println(number + " is an integer number.");
+        }
+        else{
+            System.out.println(number + " is a decimal number.");
+        }
+    }
     
 }
